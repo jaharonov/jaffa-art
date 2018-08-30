01b2250c64265b9261c1bc5dec146e5ea403e9ec
Jaffa Aharonov <jaharonov@gmail.com>
2018-08-30 17:23:42 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<template>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates" rel="stylesheet">
+
+  </head>
+  <div id="app" variant="transparent">
+    <div class="fixed-top">
+    <header>
+    <b-navbar toggleable="md" type="primary" variant="success" class="nav-text">
+  
+  <b-navbar-toggle target="nav_collapse" class="nav-items" id="menu">MENU</b-navbar-toggle>
+
+  <b-navbar-brand href="#" class="mb-5" id="h1-name">JAFFA AHARONOV</b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <!-- <b-navbar-nav>
+      <b-nav-item href="#">Link</b-nav-item>
+    </b-navbar-nav> -->
+
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+ 
+      <b-nav-item class="nav-items" href="#">PROJECTS</b-nav-item>
+      <b-nav-item class="nav-items" href="#">ABOUT</b-nav-item>
+    
+      <!-- <b-nav-form>
+        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+      </b-nav-form>
+
+      <b-nav-item-dropdown text="Lang" right>
+        <b-dropdown-item href="#">EN</b-dropdown-item>
+        <b-dropdown-item href="#">ES</b-dropdown-item>
+        <b-dropdown-item href="#">RU</b-dropdown-item>
+        <b-dropdown-item href="#">FA</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown right>
+        <template slot="button-content">
+          <em>User</em>
+        </template>
+        <b-dropdown-item href="#">Profile</b-dropdown-item>
+        <b-dropdown-item href="#">Signout</b-dropdown-item>
+      </b-nav-item-dropdown> -->
+    </b-navbar-nav>
+
+  </b-collapse>
+</b-navbar>
+</header>
+<div class="row" id="svg-thing">
+<div class="col-3">
+<svg xmlns="http://www.w3.org/2000/svg">
+                <polygon fill="#FF69B4" points="198 98,4 27,303 0,0 0" />
+            </svg>
+   </div>
+   <div class="col-3">
+<svg xmlns="http://www.w3.org/2000/svg">
+                <polygon fill="#AEFF14" points="198 98,4 27,303 0,0 0" />
+            </svg>
+   </div>
+   </div>
+ </div>  
+   
+   
+<!-- navbar-1.vue -->
+    <router-view></router-view>
+    <b-col md="4" class="fixed-bottom ml-auto">
+<svg xmlns="http://www.w3.org/2000/svg">
+                <polygon fill="#FF69B4" points="198 98,4 27,303 0,0 0" height="2%"/>
+            </svg>
+   </b-col>
+      <!-- <div class="row fixed-bottom"> -->
+      
+   
+    <footer>
+      <br/>
+       <span class="p-5"> ©JAFFA AHARONOV 2018</span>
+    </footer>
+    <!-- </div> -->
+    
+    
+  
+
+  
+</div>
+  </html>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+#navname {
+  font-size: 1.5rem;
+}
+
+#menu {
+  color: rgb(218, 248, 162);
+}
+footer {
+  position: fixed; 
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #AEFF14;
+  text-align: left;
+  font-family: 'Montserrat Alternates', sans-serif;
+
+}
+#h1-name {
+  font-size: 180%;
+  font-family: 'Audiowide', cursive;
+
+}
+
+.nav-items {
+font-family: 'Montserrat Alternates', sans-serif;
+
+}
+@import './styles/custom-bootstrap.scss';
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hello">
+    <div class="m-5">
+      <div class="row p-5"></div>
+  <img src="../assets/riotbear-10.jpg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3 pt-5" width="90%" height="auto" />
+  <img src="../assets/riotbear-9.jpg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3" width="90%" height="auto" />
+  <img src="../assets/riotbear-8.jpg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3" width="90%" height="auto" />
+  <img src="../assets/riotbear-7.jpg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3" width="90%" height="auto" />
+  <img src="../assets/riotbear-4.jpg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3" width="90%" height="auto" />
+  <img src="../assets/riotbear-3.jpg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3" width="90%" height="auto" />
+  <img src="../assets/riotbear-2.jpg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3" width="90%" height="auto" />
+</div>
+    <!-- <svg xmlns="http://www.w3.org/2000/svg"  preserveAspectRatio="none">
+                <polygon fill="#FF69B4" points="232 154,644 130,692 3,300 50,0 0" />
+            </svg> -->
+    <!-- <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul> -->
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
