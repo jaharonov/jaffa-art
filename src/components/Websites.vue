dfa1f782a6b008b6a9f1a485cf12e2f264f953f5
Jaffa Aharonov <jaharonov@gmail.com>
2018-12-28 17:42:22 -0600

    changed to justify on portfolio page

--- src/components/Websites.vue
+++ src/components/Websites.vue
@@ -5,8 +5,8 @@
         <h3 class="headings text-center">Websites</h3>
       </b-row>
       <b-row class="p-5">
-        <p class="text-left">I am available for website development and design. I can custom build an entire site for you, including e-commerce integration, etc., and am also happy to customize sites on Squarespace or other CMS platforms. Rates vary from $30-$60 per hour, with the low end being for friends and QTPOC clients. Please <a href="mailto:info@jaffaaharonov.com">email</a> me for more info and to discuss your needs for your next project!</p>
-        <p class="text-left">The examples below were built for clients using a variety of platforms. I also made this website that you are currently viewing, using Vuejs, javascript, CSS, SASS, Bootstrap, Bootstrap Vue, and HTML. 
+        <p class="text-justify">I am available for website development and design. I can custom build an entire site for you, including e-commerce integration, etc., and am also happy to customize sites on Squarespace or other CMS platforms. Rates vary from $30-$60 per hour, with the low end being for friends and QTPOC clients. Please <a href="mailto:info@jaffaaharonov.com">email</a> me for more info and to discuss your needs for your next project!</p>
+        <p class="text-justify">The examples below were built for clients using a variety of platforms. I also made this website that you are currently viewing, using Vuejs, javascript, CSS, SASS, Bootstrap, Bootstrap Vue, and HTML. 
             <b-img src="/static/smiley.gif" height="25%"/></p>
       </b-row>
       <b-row id="about">
