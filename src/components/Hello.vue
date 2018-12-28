12effd15dbde8fc62928d1b4f9654d7c64faa95c
Jaffa Aharonov <jaharonov@gmail.com>
2018-12-28 17:39:57 -0600

    changed padding

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,7 +3,7 @@
       <div class="row p-5"></div>
       <div class="row p-5">
       <div class="col-md-6">
-      <div style="padding:60% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/294866045?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+      <div style="padding:100% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/294866045?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
 <p><a href="https://vimeo.com/294866045">Wuthering Heights Onesie</a> from <a href="https://vimeo.com/user39447217">jaffa</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
   <!-- <img src="../assets/self-1-svg.svg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3 pt-5" width="90%" height="auto" /> -->
 </div>
