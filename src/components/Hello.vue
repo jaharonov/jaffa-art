d25e0f98300260cbc901461457c58716c113d72e
Jaffa Aharonov <j.aharonov@gmail.com>
2019-12-10 11:45:14 -0600

    updates

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,12 +2,13 @@
   <div class="ml-5">
       <div class="row p-5"></div>
       <div class="row p-5">
-      <div class="col-md-6">
-      <div style="padding:60% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/294866045?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
-<p><a href="https://vimeo.com/294866045">Wuthering Heights Onesie</a> from <a href="https://vimeo.com/user39447217">jaffa</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-  <!-- <img src="../assets/self-1-svg.svg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3 pt-5" width="90%" height="auto" /> -->
+      <div class="col-md-6 p-3">
+        <b-img center src="/static/gemini.gif" fluid/>
 </div>
       <div class="col-md-6 pt-2">
+        <div style="padding:60% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/294866045?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+<p><a href="https://vimeo.com/294866045">Wuthering Heights Onesie</a> from <a href="https://vimeo.com/user39447217">jaffa</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+  <!-- <img src="../assets/self-1-svg.svg" center fluid-grow  blank-color="#bbb" alt="img" class="my-3 pt-5" width="90%" height="auto" /> -->
         <b-card title="UPCOMING"
           bg-variant="primary" 
           style="opacity:.7;"
@@ -17,7 +18,7 @@
           <span class="p-5"></span>
     <p class="card-text text-justify">
       <b-list-group>
-        <b-list-group-item>February 14th, 2019: Performance in <a href="http://www.tctwentypercent.org/current-season/controlled-burn/" type="primary" target="_blank">20% Theater Company Controlled Burn</a> <i>Queer Performance for a World on Fire.</i></b-list-group-item>
+        <b-list-group-item>Jaffa is currently in rehearsals with <a href="http://www.aprilsellers.com">April Sellers Dance Collective</a></b-list-group-item>
       </b-list-group>
       <span class="p-5"></span>
       <b-list-group>
