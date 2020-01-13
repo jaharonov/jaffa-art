e065d5b22ae56a5feb3d46e6f53bbc19423a9ed5
Jaffa Aharonov <j.aharonov@gmail.com>
2020-01-13 10:25:15 -0600

    updated websites

--- src/components/Websites.vue
+++ src/components/Websites.vue
@@ -13,31 +13,37 @@
         <b-col cols="lg-6 p-3">
           <figure height="75%">
   <b-img-lazy center src="/static/mctf-screenshot.png" fluid-grow/>
-  <figcaption class="text-center"><p><a href="https://www.motorcitytapfest.com">Motor City Tap Fest</a></p></figcaption>
+  <figcaption class="text-center"><p><a href="https://www.motorcitytapfest.com" target="_blank">Motor City Tap Fest</a></p></figcaption>
   </figure>
         </b-col>
     <b-col cols="lg-6 p-3">
           <figure height="75%">
   <b-img center src="/static/nyc-chor-forum-screenshot.png" fluid-grow/>
-  <figcaption class="text-center"><p>New website to be launched soon!</p></figcaption>
+  <figcaption class="text-center"><p><a href="https://www.nycchoreographersforum.com" target="_blank">NYC Choreographer's Forum</a></p></figcaption>
   </figure>
         </b-col>
             <b-col cols="lg-6 p-3">
           <figure height="75%">
   <b-img center src="/static/taja-screenshot.png" fluid-grow/>
-  <figcaption class="text-center"><p><a href="https://www.tajawillartist.com">Taja Will</a></p></figcaption>
+  <figcaption class="text-center"><p><a href="https://www.tajawillartist.com" target="_blank">Taja Will</a></p></figcaption>
   </figure>
         </b-col>
     <b-col cols="lg-6 p-3">
           <figure height="75%">
   <b-img center src="/static/overtaps-screenshot.png" fluid-grow/>
-  <figcaption class="text-center"><p><a href="https://www.overtaps.com">OverTaps</a></p></figcaption>
+  <figcaption class="text-center"><p><a href="https://www.overtaps.com" target="_blank">OverTaps</a></p></figcaption>
   </figure>
         </b-col>
     <b-col cols="lg-6 p-3">
           <figure height="75%">
   <b-img center src="/static/north-screenshot.png" fluid-grow/>
-  <figcaption class="text-center"><p><a href="https://www.northbycurrent.com">North By Current</a></p></figcaption>
+  <figcaption class="text-center"><p><a href="https://www.northbycurrent.com" target="_blank">North By Current</a></p></figcaption>
+  </figure>
+        </b-col>
+          <b-col cols="lg-6 p-3">
+          <figure height="75%">
+  <b-img center src="/static/tdd-screenshot.png" fluid-grow/>
+  <figcaption class="text-center"><p><a href="https://www.northbycurrent.com" target="_blank">Tap Dance Detroit</a></p></figcaption>
   </figure>
         </b-col>
         </b-row>
